9e284cb4310a7fece1425b8eda6e4c873c557fb2
Oliver Gilan <olivergilan@gmail.com>
2019-12-03 17:38:42 -0500

    Created library helper function for client address struct

---
+++ socketHelper.c
@@ -0,0 +1,18 @@
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+
+struct addrinfo* addStruct(const char* host, const char* port){
+    struct addrinfo* info;
+    struct addrinfo args = {
+        .ai_flags = AI_ADDRCONFIG | AI_ALL,
+        .ai_family = AF_UNSPEC,
+        .ai_socktype = SOCK_STREAM
+    };
+
+    int res = getaddrinfo(host, port, &args, &info);
+    if(res < 0)
+        printf("getaddrinfo(): %s\n", gai_strerror(res));
+    
+    return info;
+}
