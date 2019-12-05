5c9ec2c4c4083a65f303cdffe94c3d45b0f3695c
Rishab <rishabravikumar@yahoo.com>
2019-12-05 01:31:03 -0500

    Update DUMBserver.c

--- DUMBserver.c
+++ DUMBserver.c
@@ -8,83 +8,92 @@
 
 void error(char *msg)
 {
-    perror(msg);
-    exit(1);
+	perror(msg);
+	exit(1);
 }
 
 int main(int argc, char *argv[])
 {
-    //int sockfd, newsockfd, portno, clilen;
-    //struct sockaddr_in serv_addr;
-	//struct sockaddr_in cli_addr;
-	
-					// file descriptor for our server socket
-					// file descriptor for a client socket
-					// server port to connect to
-					// utility variable - size of clientAddressInfo below
-					// utility variable - for monitoring reading/writing from/to the socket
-					// char array to store data going to and coming from the socket
+	// file descriptor for our server socket:
+	// file descriptor for a client socket:
+	// server port to connect to:
+	int sockfd, newsockfd, portno, clilen;
 
-					// Super-special secret C struct that holds address info for building our server socket
-					// Super-special secret C struct that holds address info about our client socket
-	 
-	 
-	 
-	// If the user didn't enter enough arguments, complain and exit
-    if (argc < 2)
+	struct sockaddr_in serv_addr;	// Super-special secret C struct that holds address info for building our server socket
+	struct sockaddr_in cli_addr;	// Super-special secret C struct that holds address info about our client socket
+	int addrlen;					// utility variable - size of clientAddressInfo below
+	int val;						// utility variable - for monitoring reading/writing from/to the socket
+	char buffer[1024];				// char array to store data going to and coming from the socket
+
+
+
+	if (argc < 2)					// If the user didn't enter enough arguments, complain and exit
 	{
-        fprintf(stderr,"ERROR, no port provided\n");
-        exit(1);
-    }
-	 
-	 
-	 
+		fprintf(stderr, "ERROR, no port provided\n");
+		exit(1);
+	}
+
+
+
 	/** If the user gave enough arguments, try to use them to get a port number and address **/
 
-	// convert the text representation of the port number given by the user to an int  
-	 
-	// try to build a socket .. if it doesn't work, complain and exit
-	
+	const char* portRaw = argv[1];
+	portno = atoi(portRaw);						// convert the text representation of the port number given by the user to an int  
 
-	
+
+	if ((socketfd = socket(AF_INET, SOCK_STREAM, 0)) == 0)	// try to build a socket .. if it doesn't work, complain and exit
+	{
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
+
 	/** We now have the port to build our server socket on .. time to set up the address struct **/
 
-	// zero out the socket address info struct .. always initialize!
+	memset(&serv_addr, 0, sizeof(serv_addr));	// zero out the socket address info struct .. always initialize!
+	serv_addr.sin_port = htons(portno);			// set the remote port .. translate from a 'normal' int to a super-special 'network-port-int'
+	serv_addr.sin_addr.s_addr = INADDR_ANY;		// set a flag to indicate the type of network address we'll be willing to accept connections from 
+	serv_addr.sin_family = AF_INET;				// set a flag to indicate the type of network address we'll be using 
 
-	// set the remote port .. translate from a 'normal' int to a super-special 'network-port-int'
-	 
-	// set a flag to indicate the type of network address we'll be using  
-	
-	// set a flag to indicate the type of network address we'll be willing to accept connections from
-     
-	 
-	 
+
 	/** We have an address struct and a socket .. time to build up the server socket **/
-     
-    // bind the server socket to a specific local port, so the client has a target to connect to      
-			  
-	// set up the server socket to listen for client connections
-	
-	// determine the size of a clientAddressInfo struct
-	
-	// block until a client connects, when it does, create a client socket
-	 
-	 
-	 
+
+	if (bind(sockfd, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0)	// bind the server socket to a specific local port, so the client has a target to connect to  
+	{
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+
+	if (listen(sockfd, 3) < 0)			// set up the server socket to listen for client connections
+	{
+		perror("listen");
+		exit(EXIT_FAILURE);
+	}
+
+	clilen = sizeof(cli_addr);			// determine the size of a clientAddressInfo struct
+
+
+	if ((newsockfd = accept(sockfd, (struct sockaddr *)&address, (socklen_t*)&clilen)) < 0)	// block until a client connects, when it does, create a client socket
+	{
+		perror("accept");
+		exit(EXIT_FAILURE);
+	}
+
 	/** If we're here, a client tried to connect **/
-	 
+
 	// if the connection blew up for some reason, complain and exit
-	
-	// zero out the char buffer to receive a client message
-	
-	// try to read from the client socket
-	
-	// if the read from the client blew up, complain and exit
-	
-	// try to write to the client socket
-	
-	// if the write to the client below up, complain and exit
-    
-	
-	return 0; 
+
+	buffer = { 0 };							// zero out the char buffer to receive a client message
+	val = read(newsockfd, buffer, 1024);	// try to read from the client socket
+	if (val == -1) {						// if the read from the client blew up, complain and exit
+		perror("read");
+		exit(EXIT_FAILURE);
+	}
+	// for loop to read commands and text
+
+// try to write to the client socket
+
+// if the write to the client blow up, complain and exit
+
+
+	return 0;
 }
