1b97afe4e559199a3c3ca11d83e2fcb8b0816fed
Oliver Gilan <olivergilan@gmail.com>
2019-12-08 23:13:57 -0500

    Fixed client commands scanning

--- DUMBclient.c
+++ DUMBclient.c
@@ -195,7 +195,7 @@ int main(int argc, char **argv)
 		}
 		else if (strncmp(command, "OPEN", 4) == 0 || strncmp(command, "OPNBX", 5) == 0) {
 			printf("Okay, open which message box?\nopen:> ");
-			fgets(buffer, 26, stdin);
+			fscanf(stdin, " %s", buffer);
 
 			//Check if message box name is well formed
 			if (strlen(buffer) < 5 || strlen(buffer) > 25 || !isalpha(buffer[0])) {
@@ -205,6 +205,7 @@ int main(int argc, char **argv)
 
 			//Create whole command
 			char complete[32];
+			memset(complete, 0, 32);
 			strncpy(complete, "DELBX ", 6);
 			strncat(complete, buffer, 25);
 
@@ -219,7 +220,7 @@ int main(int argc, char **argv)
 		}
 		else if (strncmp(command, "CLOSE", 5) == 0 || strncmp(command, "CLSBX", 5) == 0) {
 			printf("Okay, just confirm the name of the box you have open right now...\nclose:> ");
-			fgets(buffer, 26, stdin);
+			fscanf(stdin, " %s", buffer);
 
 			//Check if message box name is well formed
 			if (strlen(buffer) < 5 || strlen(buffer) > 25 || !isalpha(buffer[0])) {
@@ -229,6 +230,7 @@ int main(int argc, char **argv)
 
 			//Create whole command
 			char complete[32];
+			memset(complete, 0, 32);
 			strncpy(complete, "DELBX ", 6);
 			strncat(complete, buffer, 25);
 
